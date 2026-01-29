b6d13ee5c9ae54b37affb468181990b43f70e2fb
Cam <chinderliter12@gmail.com>
2026-01-29 15:04:22 -0600

    Adding Files

---
+++ confirm.php
@@ -0,0 +1,7 @@
+<html>
+    <body>
+        Name: <?php echo $_POST["name"]; ?></br>
+        Email: <?php echo $_POST["email"]; ?></br>
+        Age: <?php echo $_POST["age"]; ?>
+    </body>
+</html>
---
+++ index.php
@@ -0,0 +1,10 @@
+<html>
+    <body>
+        <form action="confirm.php" method="post">
+            Name: <input type="text" name="name"></br></br>
+            Email: <input type="text" name="email"></br></br>
+            Age: <input type="text" name="age"></br></br>
+            <input type="submit">
+        </form>
+    </body>
+</html>
